647924952a927aa6c1db1afbf33ebdefa3d59036
g0bbl3z <zvandehy@gmail.com>
2020-03-11 22:55:38 +1100

    Work on Software Design A1

--- SoftwareDesign/Assignment1/src/test/java/au/edu/sydney/pac/erp/client/ClientImplTest.java
+++ SoftwareDesign/Assignment1/src/test/java/au/edu/sydney/pac/erp/client/ClientImplTest.java
@@ -6,128 +6,130 @@ import static org.junit.Assert.*;
 public class ClientImplTest {
 
     @Test
-    public void ClientImpl(){
-        //todo: Find out if the Exceptions testing shown in the slides is better practice than instanceof
-        Client client;
+    public void ClientImpl_InvalidID() {
         //test invalid id input
         try {
-            client = new ClientImpl(0, "firstName", "lastName", "phoneNumber");
+            Client client = new ClientImpl(0, "firstName", "lastName", "phoneNumber");
             fail("id cannot be 0");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
         try {
-            client = new ClientImpl(-1, "firstName", "lastName", "phoneNumber");
+            Client client = new ClientImpl(-1, "firstName", "lastName", "phoneNumber");
             fail("id cannot be negative");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
         try {
-            client = new ClientImpl(Integer.MIN_VALUE, "firstName", "lastName", "phoneNumber");
+            Client client = new ClientImpl(Integer.MIN_VALUE, "firstName", "lastName", "phoneNumber");
             fail("id cannot be negative");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
-
+    }
+    @Test
+    public void ClientImpl_InvalidFirstName() {
         //test invalid firstName input
         try {
-            client = new ClientImpl(1, "", "lastName", "phoneNumber");
+            Client client = new ClientImpl(1, "", "lastName", "phoneNumber");
             fail("first name cannot be empty");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
         try {
-            client = new ClientImpl(1, null, "lastName", "phoneNumber");
+            Client client = new ClientImpl(1, null, "lastName", "phoneNumber");
             fail("first name cannot be null");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
-
+    }
+    @Test
+    public void ClientImpl_InvalidLastName() {
         //test invalid lastName input
         try {
-            client = new ClientImpl(1, "firstName", "", "phoneNumber");
+            Client client = new ClientImpl(1, "firstName", "", "phoneNumber");
             fail("last name cannot be empty");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
         try {
-            client = new ClientImpl(1, "firstName", null, "phoneNumber");
+            Client client = new ClientImpl(1, "firstName", null, "phoneNumber");
             fail("last name cannot be null");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
-
+    }
+    @Test
+    public void ClientImpl_InvalidPhoneNumber() {
         //test invalid phoneNumber input
         try {
-            client = new ClientImpl(1, "firstName", "lastName", "");
+            Client client = new ClientImpl(1, "firstName", "lastName", "");
             fail("phone number cannot be empty");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
         try {
-            client = new ClientImpl(1, "firstName", "lastName", null);
+            Client client = new ClientImpl(1, "firstName", "lastName", null);
             fail("phone number cannot be null");
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
         }
-
+    }
+    @Test
+    public void ClientImpl_Valid(){
         //verify that valid inputs create the corresponding Client
-        client = new ClientImpl(10, "Zeke", "Van Dehy", "7202318709");
+        Client client = new ClientImpl(10, "Zeke", "Van Dehy", "7202318709");
         assertEquals(10, client.getID());
         assertEquals("Zeke", client.getFirstName());
         assertEquals("Van Dehy", client.getLastName());
         assertEquals("7202318709", client.getPhoneNumber());
 
-        //verify id works on max int just for fun
+        //verify id works on max int
         client = new ClientImpl(Integer.MAX_VALUE, "Zeke", "Van Dehy", "7202318709");
         assertEquals(Integer.MAX_VALUE, client.getID());
-
     }
 
     @Test
-    public void assignDepartment() {
-        Client client =  new ClientImpl(1, "first", "last", "1234567890");
+    public void assignDepartment_Valid() {
+        Client client = new ClientImpl(1, "first", "last", "1234567890");
 
         //assign department to unassigned client should work
         assertFalse(client.isAssigned());
         client.assignDepartment("001");
         assertEquals("001", client.getDepartmentCode());
         assertTrue(client.isAssigned());
+    }
+    @Test (expected = IllegalStateException.class)
+    public void assignDepartment_AlreadyAssigned() {
+        Client client = new ClientImpl(1, "first", "last", "1234567890");
 
+        client.assignDepartment("001");
+        assertTrue(client.isAssigned());
         //assign department to assigned client should fail
-        try {
-            client.assignDepartment("002");
-            fail("Cannot assign department to client who is already assigned");
-        } catch (Exception e) {
-            assertTrue(e instanceof IllegalStateException);
-        }
+        client.assignDepartment("002");
         //department should not change
         assertEquals("001", client.getDepartmentCode());
         assertTrue(client.isAssigned());
-
-        //check null and empty inputs on unassigned client
-        client = new ClientImpl(2,"first2", "last2", "0123456789");
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void assignDepartment_EmptyDepartment() {
+        //check empty inputs on unassigned client
+        Client client = new ClientImpl(1, "first", "last", "1234567890");
         //department code can't be empty
-        try {
-            client.assignDepartment("");
-            fail("department code cannot be empty");
-        } catch (Exception e) {
-            assertTrue(e instanceof IllegalArgumentException);
-        }
+        client.assignDepartment("");
+        //shouldn't assign department
         assertNull(client.getDepartmentCode());
         assertFalse(client.isAssigned());
-
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void assignDepartment_NullDepartment() {
+        //check nul department code on unassigned client
+        Client client = new ClientImpl(1, "first", "last", "1234567890");
         //department code can't be null
-        try {
-            client.assignDepartment(null);
-            fail("department code cannot be null");
-        } catch (Exception e) {
-            assertTrue(e instanceof IllegalArgumentException);
-        }
+        client.assignDepartment(null);
+        //shouldn't assign department
         assertNull(client.getDepartmentCode());
         assertFalse(client.isAssigned());
-
-        //todo: should I check null and empty inputs on unassigned client?
     }
 
     @Test
@@ -143,26 +145,18 @@ public class ClientImplTest {
     }
 
     @Test
-    public void getDepartmentCode() {
+    public void getDepartmentCode_Unassigned() {
         Client client = new ClientImpl(1, "first", "last", "1234567890");
 
         //unassigned client should return null
         assertNull(client.getDepartmentCode());
-
+    }
+    @Test
+    public void getDepartmentCode_Valid() {
+        Client client = new ClientImpl(1, "first", "last", "1234567890");
         //assigned client should return the department code
         client.assignDepartment("001");
         assertEquals("001", client.getDepartmentCode());
-
-        //reassigning a client should not work (return same department code)
-        try {
-            client.assignDepartment("002");
-            fail("Cannot assign department to client who is already assigned");
-        } catch (Exception e) {
-            assertTrue(e instanceof IllegalStateException);
-        }
-        //department should not change
-        assertEquals("001", client.getDepartmentCode());
-        assertTrue(client.isAssigned());
     }
 
     @Test
--- SoftwareDesign/Assignment1/src/test/java/au/edu/sydney/pac/erp/client/ClientListImplTest.java
+++ SoftwareDesign/Assignment1/src/test/java/au/edu/sydney/pac/erp/client/ClientListImplTest.java
@@ -2,10 +2,98 @@ package au.edu.sydney.pac.erp.client;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 public class ClientListImplTest {
 
     @Test
-    public void addClient() {
+    public void testAddClient_InvalidID() {
+        ClientListImpl clientList = new ClientListImpl();
+        //test invalid id input
+        try {
+            Client client = clientList.addClient(0, "firstName", "lastName", "phoneNumber");
+            fail("id cannot be 0");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+        try {
+            Client client = clientList.addClient(-1, "firstName", "lastName", "phoneNumber");
+            fail("id cannot be negative");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+    @Test (expected = IllegalStateException.class)
+    public void testAddClient_DuplicateID() {
+        ClientListImpl clientList = new ClientListImpl();
+
+        Client client = clientList.addClient(1, "firstName", "lastName", "phoneNumber");
+        Client client2 = clientList.addClient(1, "firstName", "lastName", "phoneNumber");
+    }
+    @Test
+    public void testAddClient_InvalidFirstName() {
+        ClientListImpl clientList = new ClientListImpl();
+        //test invalid firstName input
+        try {
+            Client client = clientList.addClient(1, "", "lastName", "phoneNumber");
+            fail("first name cannot be empty");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+        try {
+            Client client = clientList.addClient(1, null, "lastName", "phoneNumber");
+            fail("first name cannot be null");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+    @Test
+    public void testAddClient_InvalidLastName() {
+        ClientListImpl clientList = new ClientListImpl();
+        //test invalid lastName input
+        try {
+            Client client = clientList.addClient(1, "firstName", "", "phoneNumber");
+            fail("last name cannot be empty");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+        try {
+            Client client = clientList.addClient(1, "firstName", null, "phoneNumber");
+            fail("last name cannot be null");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+    @Test
+    public void testAddClient_InvalidPhoneNumber() {
+        ClientListImpl clientList = new ClientListImpl();
+        //test invalid phoneNumber input
+        try {
+            Client client = clientList.addClient(1, "firstName", "lastName", "");
+            fail("phone number cannot be empty");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+        try {
+            Client client = clientList.addClient(1, "firstName", "lastName", null);
+            fail("phone number cannot be null");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+    @Test
+    public void testAddClient_Valid(){
+        ClientListImpl clientList = new ClientListImpl();
+        Client client = clientList.addClient(10, "first", "last", "1234567890");
+        //verify that the client was created
+        assertEquals(10, client.getID());
+        assertEquals("first", client.getFirstName());
+        assertEquals("last", client.getLastName());
+        assertEquals("1234567890", client.getPhoneNumber());
+        //verify that the client was added
+        assertEquals(1, clientList.findAll().size());
+        assertTrue(clientList.findAll().contains(client));
     }
 
     @Test
