efdd7fbff6aa8f99d2456f62172ed2a5fa7bf703
g0bbl3z <zvandehy@gmail.com>
2020-04-29 15:26:16 -0600

    Finish Software Design HW T8 Concurrency

---
+++ SoftwareDesign/HW9_Concurrency/src/main/java/Counter.java
@@ -0,0 +1,53 @@
+public class Counter implements Runnable{
+    String prefix;
+    int start;
+    int finish;
+    boolean reverse;
+    Printer printer;
+
+    public Counter(String prefix, int start, int finish, boolean reverse, Printer printer) {
+        if(start <= finish) {
+            if(prefix != null && !prefix.equals("")) {
+                if(printer != null) {
+                    this.prefix = prefix;
+                    this.start = start;
+                    this.finish = finish;
+                    this.reverse = reverse;
+                    this.printer = printer;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if(reverse){
+            for(int i=finish; i>=start; i--) {
+                printer.lock.lock();
+                printer.setPrefix(prefix);
+                printer.setCount(i);
+                printer.print();
+                printer.lock.unlock();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            printer.setCount(start);
+            for(int i=start; i<=finish; i++) {
+                printer.lock.lock();
+                printer.setPrefix(prefix);
+                printer.setCount(i);
+                printer.print();
+                printer.lock.unlock();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ SoftwareDesign/HW9_Concurrency/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Thread thread1 = new Thread(new Counter("Deadlocks avoided: ", 0,100,false, myPrinter));
+        Thread thread2 = new Thread(new Counter("Threads starved: ", 0,100,true, myPrinter));
+
+        thread1.start();
+        thread2.start();
+    }
+}
